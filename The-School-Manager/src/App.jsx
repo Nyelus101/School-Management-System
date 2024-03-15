e364d365708b9e3ba022cd27d8ff7d3cc1567182
Nyelus101 <cmuonyelu@gmail.com>
2024-03-15 06:39:14 +0100

    update

--- The-School-Manager/src/App.jsx
+++ The-School-Manager/src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
+
 
 
 const App = () => {
@@ -11,8 +13,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
-        <Title />
+        <Title subTitle='OUR PROGRAM' title='What We Offer'/>
         <Programs />
+        <About />
       </div>
     </div>
   )
