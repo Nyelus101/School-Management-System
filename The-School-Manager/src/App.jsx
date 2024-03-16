ae23aad06834261c1a5dfe4c9544fd1aff77867d
Nyelus101 <cmuonyelu@gmail.com>
2024-03-16 16:55:07 +0100

    update

--- The-School-Manager/src/App.jsx
+++ The-School-Manager/src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 
 
@@ -16,6 +18,10 @@ const App = () => {
         <Title subTitle='OUR PROGRAM' title='What We Offer'/>
         <Programs />
         <About />
+        <Title subTitle='Gallery' title='Campus Photos'/>
+        <Campus />
+        <Title subTitle='TESTIMONIALS' title='What Students Say' />
+        <Testimonials />
       </div>
     </div>
   )
