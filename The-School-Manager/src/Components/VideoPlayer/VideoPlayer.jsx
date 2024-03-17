ebe962c127c8ed0ec79e4d1844ea860924304040
Nyelus101 <cmuonyelu@gmail.com>
2024-03-17 11:57:16 +0100

    update

--- The-School-Manager/src/Components/VideoPlayer/VideoPlayer.jsx
+++ The-School-Manager/src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,13 +1,13 @@
-// import React from 'react'
-// import './VideoPlayer.css'
+import React from 'react'
+import './VideoPlayer.css'
+import video from '../../assets/college-video.mp4'
 
+const VideoPlayer = () => {
+  return (
+    <div className='video-player'>
+        <video src={video} autoPlay muted controls></video>
+    </div>
+  )
+}
 
-// const VideoPlayer = () => {
-//   return (
-//     <div className='video-player'>
-//         <video src={}></video>
-//     </div>
-//   )
-// }
-
-// export default VideoPlayer
+export default VideoPlayer
